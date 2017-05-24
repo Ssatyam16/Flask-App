92e1cac5428c718b1886ed0a9d0464c0552fdc50
Satyam Sagar <satyamsagar291@gmail.com>
2017-05-25 01:20:51 +0530

    intial

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles() :
+    articles = [
+         {
+             'id': 1,
+             'title': 'Article One',
+             'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec sapien elit. Nulla et elit a eros iaculis egestas. Mauris ac porta felis. Maecenas massa leo, tempus a quam id, rhoncus dictum quam. Curabitur dui urna, ornare in rhoncus at, feugiat eget est. Vestibulum sed sapien nisl. In hac habitasse platea dictumst. Nulla et dapibus nulla, sed semper neque. Suspendisse ac porta lorem. In vitae leo pretium ipsum accumsan lacinia.',
+             'author':'satyam',
+             'create_date':'4-5-17'
+         },
+         {
+             'id': 2,
+             'title': 'Article Two',
+             'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec sapien elit. Nulla et elit a eros iaculis egestas. Mauris ac porta felis. Maecenas massa leo, tempus a quam id, rhoncus dictum quam. Curabitur dui urna, ornare in rhoncus at, feugiat eget est. Vestibulum sed sapien nisl. In hac habitasse platea dictumst. Nulla et dapibus nulla, sed semper neque. Suspendisse ac porta lorem. In vitae leo pretium ipsum accumsan lacinia.',
+             'author':'sagar',
+             'create_date':'2-5-17'
+         },
+         {
+             'id': 3,
+             'title': 'Article Three',
+             'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec sapien elit. Nulla et elit a eros iaculis egestas. Mauris ac porta felis. Maecenas massa leo, tempus a quam id, rhoncus dictum quam. Curabitur dui urna, ornare in rhoncus at, feugiat eget est. Vestibulum sed sapien nisl. In hac habitasse platea dictumst. Nulla et dapibus nulla, sed semper neque. Suspendisse ac porta lorem. In vitae leo pretium ipsum accumsan lacinia.',
+             'author':'satyam',
+             'create_date':'24-5-17'
+         }
+    ]
+    return articles
